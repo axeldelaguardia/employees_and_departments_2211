e7e0ef40b672a7aa9c0db7cb6fee8c0396b2412d
Axel De La Guardia <axeldelaguardia@gmail.com>
2023-01-13 09:11:39 -0700

    Add: set up and #initialize test for Department

---
+++ spec/department_spec.rb
@@ -0,0 +1,18 @@
+require './lib/spec_helper'
+
+describe Department do
+	let(:customer_service) {Department.new("Customer Service")}
+	let(:bobbi) {Employee.new({name: "Bobbi Jaeger", age: "30", salary: "100000"})}
+	let(:aaron) {Employee.new({name: "Aaron Tanaka", age: "25", salary: "90000"}) }
+
+	describe '#initialize' do
+		it 'exists' do
+			expect(customer_service).to be_a(Department)
+		end
+
+		it 'has attributes' do
+			expect(customer_service.name).to eq("Customer Service")
+			expect(customer_service.employees).to eq([])
+		end
+	end
+end
