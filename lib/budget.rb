3fc753d37d12c23b7fae7a90cc8a9b758d53c108
Axel De La Guardia <axeldelaguardia@gmail.com>
2023-01-13 09:42:14 -0700

    Add: #dept_with_expenses_under method

--- lib/budget.rb
+++ lib/budget.rb
@@ -10,4 +10,13 @@ class Budget
 	def add_department(department)
 		@departments << department
 	end
+
+	def dept_with_expenses_under(amount)
+		returned_departments = Hash.new(0)
+		@departments.each do |department|
+			expenses = department.expenses
+			returned_departments[department] = expenses if expenses < amount
+		end
+		returned_departments
+	end
 end
