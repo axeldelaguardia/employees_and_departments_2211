dbc5275d166d7b97ba4098a3cf35a7b105475937
Axel De La Guardia <axeldelaguardia@gmail.com>
2023-01-13 11:52:09 -0700

    Add: #record_employee_expense method

--- lib/department.rb
+++ lib/department.rb
@@ -2,13 +2,15 @@ class Department
 	attr_reader :name,
 							:employees,
 							:expenses,
-							:employee_expenses
+							:employee_expenses,
+							:emp_expenses
 
 	def initialize(name)
 		@name = name
 		@employees = []
 		@expenses = 0
 		@employee_expenses = Hash.new(0)
+		@emp_expenses = Hash.new{|k, v| k[v] = []}
 	end
 
 	def hire(employee)
@@ -18,5 +20,10 @@ class Department
 	def expense(amount, employee)
 		@expenses += amount
 		employee_expenses[employee] += amount
+		record_employee_expense(amount, employee)
+	end
+
+	def record_employee_expense(amount, employee)
+		@emp_expenses[employee] << amount
 	end
 end
