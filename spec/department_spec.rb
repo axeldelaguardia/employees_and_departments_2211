7e2e0cfa72de82c95de64870b4bb512a0906c42e
Axel De La Guardia <axeldelaguardia@gmail.com>
2023-01-13 09:16:32 -0700

    Add: tests for attr expenses and #expense

--- spec/department_spec.rb
+++ spec/department_spec.rb
@@ -13,6 +13,7 @@ describe Department do
 		it 'has attributes' do
 			expect(customer_service.name).to eq("Customer Service")
 			expect(customer_service.employees).to eq([])
+			expect(customer_service.expenses).to eq(0)
 		end
 	end
 
@@ -22,6 +23,15 @@ describe Department do
 			customer_service.hire(aaron)
 
 			expect(customer_service.employees).to eq([bobbi, aaron])
+		end
+	end
+
+	describe '#expense' do
+		it 'expenses to the department' do
+			customer_service.expense(100)
+			customer_service.expense(25)
+
+			expect(customer_service.expenses).to eq(125)
 		end
 	end
 end
