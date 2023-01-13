f088f3ac8aaeccd021c466d1eba7dff2ad99a0af
Axel De La Guardia <axeldelaguardia@gmail.com>
2023-01-13 10:19:42 -0700

    Add: employee_expenses attribute and change expense method

--- lib/department.rb
+++ lib/department.rb
@@ -1,19 +1,22 @@
 class Department
 	attr_reader :name,
 							:employees,
-							:expenses
+							:expenses,
+							:employee_expenses
 
 	def initialize(name)
 		@name = name
 		@employees = []
 		@expenses = 0
+		@employee_expenses = Hash.new(0)
 	end
 
 	def hire(employee)
 		@employees << employee
 	end
 
-	def expense(amount)
+	def expense(amount, employee)
 		@expenses += amount
+		employee_expenses[employee] += amount
 	end
 end
