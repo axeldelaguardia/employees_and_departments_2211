1ac9540eadae89f365b5fc93b524268116d9674b
Axel De La Guardia <axeldelaguardia@gmail.com>
2023-01-13 10:16:23 -0700

    Add: Second argument of employee to #expense for Department and #employee_expenses test

--- spec/department_spec.rb
+++ spec/department_spec.rb
@@ -28,10 +28,30 @@ describe Department do
 
 	describe '#expense' do
 		it 'expenses to the department' do
-			customer_service.expense(100)
-			customer_service.expense(25)
+			customer_service.expense(100, bobbi)
+			customer_service.expense(25, aaron)
 
 			expect(customer_service.expenses).to eq(125)
+		end
+	end
+
+	describe '#employee_expenses' do
+		it 'returns a hash with employee name as key and employee expenses as value' do
+			expect(customer_service.employee_expenses).to eq({bobbi => 0, aaron => 0})
+
+			customer_service.expense(100, bobbi)
+			customer_service.expense(25, aaron)
+			
+			expected = {bobbi => 100, aaron => 25}
+
+			expect(customer_service.employee_expenses).to eq(expected)
+
+			customer_service.expense(50, aaron)
+
+			expected = {bobbi => 100, aaron => 75}
+
+			expect(customer_service.expenses).to eq(175)
+			expect(customer_service.eemployee_expenses).to eq(expected)
 		end
 	end
 end
